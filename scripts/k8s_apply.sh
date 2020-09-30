efa4e1cce590a3e94f0268e35406c88ed376e1c6
shintaro uchiyama <shintaro.uchiyama@jdsc.ai>
2020-09-30 23:24:13 +0900

    create golang k8s manifest

--- scripts/k8s_apply.sh
+++ scripts/k8s_apply.sh
@@ -1,4 +1,3 @@
 #!/bin/zsh
-echo "password=$(gcloud secrets versions access latest --secret="mysql" | jq -r '.value')" > deployments/kubernetes/mysql/password.txt
+gcloud secrets versions access latest --secret="mysql" | jq -r '.value' > deployments/kubernetes/mysql/password
 kubectl apply -k ./deployments/kubernetes/mysql
-kubectl apply -f ./deployments/kubernetes/mysql
